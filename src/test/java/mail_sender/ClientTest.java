3bd624c86259cea8cdeba75e22a838c693b74f7a
kkulykk <kkulykk@gmail.com>
2021-11-25 00:13:06 +0200

    Update tests

--- src/test/java/mail_sender/ClientTest.java
+++ src/test/java/mail_sender/ClientTest.java
@@ -1,31 +1,32 @@
 package mail_sender;
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class ClientTest {
     Client client;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
         client = new Client("Ben", Gender.MALE, 45, "hello@a.bc");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getName() {
         assertEquals(client.getName(), "Ben");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getSex() {
         assertEquals(client.getSex(), Gender.MALE);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getAge() {
         assertEquals(client.getAge(), 45);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getEmail() {
         assertEquals(client.getEmail(), "hello@a.bc");
     }
